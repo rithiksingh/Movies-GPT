6c063722fb0a5735d1c49fd54713af2fa02882d9
Hrithik Singh <rithiksingh666@gmail.com>
2024-09-27 12:18:15 +0530

    done firebase setup and deployed

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAG_SnNWW94b2kU552JNJ2s4pBk9ZHJMaE",
+  authDomain: "moviesgpt-334e8.firebaseapp.com",
+  projectId: "moviesgpt-334e8",
+  storageBucket: "moviesgpt-334e8.appspot.com",
+  messagingSenderId: "72680235462",
+  appId: "1:72680235462:web:e6b3d5d744d39bb0b9d2c7",
+  measurementId: "G-4PYZ4GCQJP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
