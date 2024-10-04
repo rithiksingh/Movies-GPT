e67f8f92e91ffac699ec71d12101607b96a1dd17
Hrithik Singh <rithiksingh666@gmail.com>
2024-10-04 19:33:08 +0530

    moved the links to constants.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../utils/userSlice';
+import { LOGO } from '../utils/constants';
 const Header = () => {
   const user = useSelector((store) => store.user);
   console.log(user);
@@ -45,7 +46,7 @@ const Header = () => {
       <div className="absolute z-10 px-8 py-2 bg-gradient-to-b from-black w-full flex justify-between items-center p2">
         <img
           className="w-44"
-          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          src={LOGO}
           alt="Netflix-logo"
         />
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,6 +5,7 @@ import { auth } from "../utils/firebase";
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { BACKGROUND } from "../utils/constants";
 
 const Login = () => {
     const [isSignInForm,setIsSignInForm]= useState(true);
@@ -33,7 +34,7 @@ const Login = () => {
               // Signed up
               const user = userCredential.user;
               updateProfile(user, {
-                displayName: name.current.value, photoURL: "https://example.com/jane-q-user/profile.jpg"
+                displayName: name.current.value, photoURL: ""
               }).then(() => {
                 // Profile updated!
                 const { uid, email, displayName } = auth.currentUser;
@@ -82,8 +83,8 @@ const Login = () => {
       <Header />
       <div className="absolute">
         <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/bfc0fc46-24f6-4d70-85b3-7799315c01dd/web/IN-en-20240923-TRIFECTA-perspective_74e21c19-980e-45ef-bd6c-78c1a6ce9381_large.jpg"
-          alt="logo"
+          src={BACKGROUND}
+          alt="background"
         />
       </div>
       <form onSubmit={(e)=> e.preventDefault()} 
