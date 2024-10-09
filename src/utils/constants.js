32d20d39798325bbddc98f6687231bdd599fac18
Hrithik Singh <rithiksingh666@gmail.com>
2024-10-09 18:19:21 +0530

    integerated TMDB with our app

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 export const LOGO="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const BACKGROUND="https://assets.nflxext.com/ffe/siteui/vlv3/bfc0fc46-24f6-4d70-85b3-7799315c01dd/web/IN-en-20240923-TRIFECTA-perspective_74e21c19-980e-45ef-bd6c-78c1a6ce9381_large.jpg";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODlmOWJkN2QwYzE5YjZkODI1NjVmNTMzYmQ5YWJkNSIsIm5iZiI6MTcyODA1MTI4MC41MTU4MDksInN1YiI6IjY2ZmRiY2E1ZTQ4MDE0OTE0Njg0YmJkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BnNcmqnCo0SXnrPTVqxADXC8OErzPw1wiMbX4mT44Ck'
+    }
+  };
